00f228db5aff57476ddc80a1d1d57d3e710f8568
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-06-03 15:52:05 -0400

    All Tests Pass

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+class Polygon{
+  constructor(sides){
+    this.sides = sides
+  }
+
+  get countSides(){
+    return this.sides.length
+  }
+
+  get perimeter(){
+    return this.sides.reduce((acc, item) => acc + item)
+  }
+}
+
+class Triangle extends Polygon{
+  get isValid(){
+    if (this.sides.length !== 3) {
+      return false;
+    }
+    const [a, b, c] = this.sides
+    return a + b > c && a + c > b && c + b > a
+  }
+}
+
+class Square extends Polygon{
+  get isValid(){
+    if(this.sides.length !== 4){
+      return false
+    }
+    const [a, b, c, d] = this.sides
+    return a === b && c === d && c === d
+  }
+
+  get area(){
+    return this.sides[0] ** 2
+  }
+}
